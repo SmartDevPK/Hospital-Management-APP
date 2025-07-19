608529cf766d69d513ff07870c65dee299678bad
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-19 23:55:05 +0100

    Connect To MongoDB

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-require('dotenv').config()
+// Load environment variables
+require('dotenv').config();
 
+// Import dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const app = express ()
+// Initialize the app
+const app = express();
 
-const PORT = process.env.PORT || 5000
+// Middleware
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+// Port
+const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
-
-
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on http:localhst:${PORT}`);
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
