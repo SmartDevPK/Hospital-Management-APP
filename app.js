702ac02b971570ea9619949dd7f13f48d2b68f94
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-19 23:47:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express ()
+
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http:localhst:${PORT}`);
+})
